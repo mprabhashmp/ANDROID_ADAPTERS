ff395638f4b2f2eab036333de6afee4d61b592fc
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2024-02-09 13:23:34 +0530

    ex33

---
+++ EX2_Array Adapter/app/src/main/java/com/mad/lb06_ex2/simple_spinner_item.java
@@ -0,0 +1,14 @@
+package com.mad.lb06_ex2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class simple_spinner_item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.simple_spinner_item);
+    }
+}
