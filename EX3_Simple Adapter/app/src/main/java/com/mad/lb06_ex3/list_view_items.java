e967566f70b3a735216e001bf0f60445cc8cc086
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2024-02-10 14:16:34 +0530

    ex22

---
+++ EX3_Simple Adapter/app/src/main/java/com/mad/lb06_ex3/list_view_items.java
@@ -0,0 +1,14 @@
+package com.mad.lb06_ex3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class list_view_items extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_view_items);
+    }
+}
